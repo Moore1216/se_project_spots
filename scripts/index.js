7bf54bf67e44810597102e8c8c8b605c6bfc83cc
Chase Moore <taz.johnson28@gmail.com>
2026-02-25 18:58:22 -0500

    modified:   index.html 	modified:   scripts/index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,29 +2,24 @@ const editProfileBtn = document.querySelector('.profile__edit-btn');
 const editProfileModal = document.querySelector('#edit-profile-modal');
 const editProfileBtnClose = editProfileModal.querySelector('.modal__close-btn');
 
-
-const newPostBtn = document.querySelector('.profile__new-post-btn');
+const newPostBtn = document.querySelector('.profile__add-btn'); // FIXED
 const newPostModal = document.querySelector('#new-post-modal');
 const newPostBtnClose = newPostModal.querySelector('.modal__close-btn');
 
 
-editProfileModal.addEventListener('click', function () {
+editProfileBtn.addEventListener('click', function () {
     editProfileModal.classList.add('modal_is-opened');
-})
-
-
-editProfileBtnClose.addEventListener('click', function (){ 
-    editProfileModal.classList.remove('modal_is-opened');
-} )
+});
 
 newPostBtn.addEventListener('click', function () {
     newPostModal.classList.add('modal_is-opened');
-})
+});
 
-editProfileBtnClose.addEventListener('click', function (){ 
+
+editProfileBtnClose.addEventListener('click', function () {
     editProfileModal.classList.remove('modal_is-opened');
-} )
+});
 
-newPostBtnClose.addEventListener('click', function (){ 
+newPostBtnClose.addEventListener('click', function () {
     newPostModal.classList.remove('modal_is-opened');
-} )
+});
